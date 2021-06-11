e44ef9af18cff3b7b54b175a03d1f6ec7080c3f5
ayanda qoseem <kayceayanda11@gmail.com>
2021-06-11 08:47:52 +0100

    first commit

---
+++ assets/JS/script.js
@@ -0,0 +1,29 @@
+function formProcess() {
+    fullname= document.getElementById('fullName').value
+    email= document.getElementById('email').value
+    phone= document.getElementById('phone').value 
+
+    // var data= {
+    //     "fullName": fullname,
+    //     "email": email,
+    //     "phone": phone,
+    //     "isEnabled":true,
+    //     "roles":["user"]
+    // }
+    // if(phone!=phone){
+    //     alert('error')
+    // }else{
+    //     fetch('https://api.openclass.ng/Register', {
+    //     method: 'POST',
+    //     body: JSON.stringify(data),
+    //     headers: {
+    //         'content-Type':'application/json'
+    //     }
+    // }).then(function(data) {
+    //     alert('Congratulations')
+    // })
+    // }
+
+    alert('congrats')
+
+}
